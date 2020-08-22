2fd2364ea5a759d0f31b4d867ec3fb60ef179d9a
Victor Massé <victor.dhamma@gmail.com>
2020-08-22 13:26:19 +0200

    clean up fauna file

--- assets/scripts/fauna.js
+++ assets/scripts/fauna.js
@@ -1,12 +1,6 @@
 // import faunadb, { query as q } from "faunadb"
-// console.log(q)
-// > JSBUILD: failed to transform "scripts/fauna.js" (application/javascript): Could not resolve "util"
-
 // import faunadb, { query as q } from "../../node_modules/faunadb/dist/faunadb-min.js"
-// console.log(q)
-// > Uncaught ReferenceError: q is not defined
 
 import faunadb, { query } from "../../node_modules/faunadb/dist/faunadb-min.js"
 q = query
-console.log(q)
-// > OK
+
