b9ac936787460b2ccb357ad2a1eebcecf438400c
Victor Massé <victor.dhamma@gmail.com>
2020-08-22 12:56:38 +0200

    prepare jsbuild

---
+++ assets/scripts/fauna.js
@@ -0,0 +1,7 @@
+// import faunadb, { query as q } from "faunadb"
+// import faunadb, { query as q } from "../../node_modules/faunadb/dist/faunadb-min.js"
+import faunadb, { query } from "../../node_modules/faunadb/dist/faunadb-min.js"
+q = query;
+
+console.log(faunadb);
+console.log(q);
