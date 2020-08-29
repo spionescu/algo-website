d1bfc802dcc86e951028b219fd95e80f7afd6b0c
Victor Massé <victor.dhamma@gmail.com>
2020-08-29 09:26:00 +0200

    move file

---
+++ assets/scripts/functions/fauna.js
@@ -0,0 +1,10 @@
+// import faunadb, { query as q } from "../../node_modules/faunadb/dist/faunadb-min.js"
+// import faunadb, { query } from "../../node_modules/faunadb/dist/faunadb-min.js"
+
+// q = query
+// fauna = faunadb
+
+import faunadb, { query as q } from "faunadb"
+
+console.log(faunadb)
+console.log(q)
